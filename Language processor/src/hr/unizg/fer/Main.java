d29b508773d3cf0e85f853e71cc616e564c885cc
Dean Babic <deanbabic@hotmail.com>
2014-10-11 14:45:02 +0200

    Trying the git in eclipse

--- Language processor/src/hr/unizg/fer/Main.java
+++ Language processor/src/hr/unizg/fer/Main.java
@@ -1,10 +1,15 @@
 package hr.unizg.fer;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a =5;
+		Scanner reader = new Scanner(System.in);
+		String output=reader.next();
+		System.out.print(output);
+		
 	}
 
 }
